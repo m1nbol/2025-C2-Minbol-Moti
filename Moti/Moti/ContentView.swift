6710d008dff69207782ae39666862aff62d3cbff
BoMin <bmlee@postech.ac.kr>
2025-04-15 23:03:32 +0900

    ⚡️ Fix ContentView

--- Moti/Moti/ContentView.swift
+++ Moti/Moti/ContentView.swift
@@ -8,14 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var router = NavigationRouter()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack(path: $router.path) {
+            VStack(spacing: 20) {
+                Button("GO TO BOARDS VIEW") {
+                    router.push(.board)
+                }
+                Button("Go to details") {
+                    router.push(.goalDetails)
+                }
+                Button("Go to GoalCompose") {
+                    router.presentSheet(.goalCompose)
+                }
+                Button("Go to TaskCompose") {
+                    router.presentSheet(.taskCompose)
+                }
+            }
+            .navigationDestination(for: Route.self) { route in
+                switch route {
+                case .board:
+                    BoardView(router: $router)
+//                case .goalCompose:
+//                    GoalComposeView(router: $router)
+                case .goalDetails:
+                    GoalDetailsView()
+//                case .taskCompose:
+//                    TaskComposeView()
+                }
+            }
+            .navigationTitle("Home")
         }
-        .padding()
+        .sheet(item: $router.presentedSheet) { sheet in
+            switch sheet {
+            case .goalCompose:
+                GoalComposeView(router: $router)
+            case .taskCompose:
+                TaskComposeView(router: $router)
+            }
+        }
     }
 }
 
