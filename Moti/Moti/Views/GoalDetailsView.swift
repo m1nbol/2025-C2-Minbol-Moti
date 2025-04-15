650c5a7fe02d5b59124478028ec9019656543469
BoMin <bmlee@postech.ac.kr>
2025-04-15 22:58:55 +0900

    ✨ Create GoalDetailsView

---
+++ Moti/Moti/Views/GoalDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  GoalDetailsView.swift
+//  Moti
+//
+//  Created by BoMin Lee on 4/15/25.
+//
+
+import SwiftUI
+
+struct GoalDetailsView: View {
+    var body: some View {
+        Text("this is goal details view")
+    }
+}
+
+#Preview {
+    GoalDetailsView()
+}
