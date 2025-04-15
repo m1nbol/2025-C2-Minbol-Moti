9deb90ab7db403700fb9cb928175d5cb58f170af
BoMin <bmlee@postech.ac.kr>
2025-04-15 21:52:26 +0900

    ✨ Navigation Router Setup

---
+++ Moti/Moti/Navigation/NavigationRouter.swift
@@ -0,0 +1,30 @@
+//
+//  NavigationRouter.swift
+//  Moti
+//
+//  Created by BoMin Lee on 4/15/25.
+//
+
+import Foundation
+import SwiftUI
+import Observation
+
+@Observable
+class NavigationRouter {
+    // 네비게이션 경로 저장 변수
+    var path = NavigationPath()
+    
+    func push(_ route: Route) {
+        path.append(route)
+    }
+    
+    func pop() {
+        if !path.isEmpty {
+            path.removeLast()
+        }
+    }
+    
+    func reset() {
+        path = NavigationPath()
+    }
+}
---
+++ Moti/Moti/Navigation/Route.swift
@@ -0,0 +1,16 @@
+//
+//  Route.swift
+//  Moti
+//
+//  Created by BoMin Lee on 4/15/25.
+//
+
+import Foundation
+import SwiftUI
+
+enum Route: Hashable {
+    case board
+    case goalCompose
+    case goalDetail
+    case todoCompose
+}
