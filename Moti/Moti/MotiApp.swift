8d4ff3f65ffacb3cfbe8fd94edc091315ece6b93
BoMin <bmlee@postech.ac.kr>
2025-04-15 16:46:29 +0900

    🎉 Generate Moti

---
+++ Moti/Moti/MotiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotiApp.swift
+//  Moti
+//
+//  Created by BoMin Lee on 4/15/25.
+//
+
+import SwiftUI
+
+@main
+struct MotiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
