830bcf6376f57bbf92b1c795808122842c790d98
BoMin <bmlee@postech.ac.kr>
2025-04-15 21:35:09 +0900

    💄 Add FontManager

---
+++ Moti/Moti/Resources/Fonts/FontManager.swift
@@ -0,0 +1,46 @@
+//
+//  FontManager.swift
+//  Moti
+//
+//  Created by BoMin Lee on 4/15/25.
+//
+
+import Foundation
+import SwiftUI
+
+extension Font {
+    enum Pretend {
+        case extraBold
+        case bold
+        case semibold
+        case medium
+        case regular
+        case light
+        
+        var value: String {
+            switch self {
+            case .extraBold:
+                return "Pretendard-ExtraBold"
+            case .bold:
+                return "Pretendard-Bold"
+            case .semibold:
+                return "Pretendard-SemiBold"
+            case .medium:
+                return "Pretendard-Medium"
+            case .regular:
+                return "Pretendard-Regular"
+            case .light:
+                return "Pretendard-Light"
+            }
+        }
+    }
+    
+    static func pretend(type: Pretend, size: CGFloat) -> Font {
+        return .custom(type.value, size: size)
+    }
+    
+    static var mainTextSemiBold34: Font {
+        return .pretend(type: .semibold, size: 34)
+    }
+
+}
