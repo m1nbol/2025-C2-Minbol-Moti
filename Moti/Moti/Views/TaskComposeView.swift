293d1c0a310d2490c4b7258760a23ac14f107bea
BoMin <bmlee@postech.ac.kr>
2025-04-21 20:42:11 +0900

    ✨ Feat TaskComposeView

--- Moti/Moti/Views/TaskComposeView.swift
+++ Moti/Moti/Views/TaskComposeView.swift
@@ -9,12 +9,18 @@ import SwiftUI
 
 struct TaskComposeView: View {
     @Binding var router: NavigationRouter
+    @Bindable var goal: Goal
+    
+    @Environment(\.modelContext) private var modelContext
+    
+    @State private var taskContext: String = ""
+    @State private var isChecked: Bool = false
     
     var body: some View {
         NavigationStack {
             VStack {
-                Text("enter your task")
-                    .foregroundStyle(.mainGray)
+                TextField("Enter your Task", text: $taskContext)
+                    .autocorrectionDisabled()
                     .font(.mainTextSemiBold34)
             }
             .toolbar {
@@ -24,13 +30,32 @@ struct TaskComposeView: View {
                     } label: {
                         HStack {
                             Image(systemName: "chevron.left")
-                            Text("Your Goal")
+                            Text("Goals")
                         }
-                        .foregroundStyle(.mainBlue)
+                    }
+                }
+                ToolbarItem(placement: .confirmationAction) {
+                    Button {
+                        // 저장해야 함
+                        saveTask()
+                        router.dismissSheet()
+                    } label: {
+                        Text("Save")
                     }
                 }
             }
+            .foregroundStyle(.mainBlue)
         }
+    }
+    
+    private func saveTask() {
+        let newTask = Task(
+            id: UUID(),
+            context: taskContext,
+            isChecked: isChecked
+        )
+        modelContext.insert(newTask)
+        goal.addTask(newTask)
     }
 }
 
