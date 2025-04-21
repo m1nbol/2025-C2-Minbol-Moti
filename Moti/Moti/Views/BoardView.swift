d4300779a68e6cda2dcd4b8eb81c27b21b4c1940
BoMin <bmlee@postech.ac.kr>
2025-04-21 20:40:04 +0900

    🎨 Fix BoardView

--- Moti/Moti/Views/BoardView.swift
+++ Moti/Moti/Views/BoardView.swift
@@ -6,20 +6,90 @@
 //
 
 import SwiftUI
+import UIKit
+import SwiftData
 
 struct BoardView: View {
-    @Binding var router: NavigationRouter
+    @Environment(\.modelContext) private var modelContext
+    
+    @State private var router = NavigationRouter()
+    @Query/*(sort: \Goal.createdAt, order: .reverse)*/ private var goals: [Goal]
+    
+    @State private var goalToDelete: Goal?
+    @State private var showDeleteAlert: Bool = false
+
+    
+    init() {
+        UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: UIColor.mainBlue]
+    }
     
     var body: some View {
-        VStack {
-            Text("this is board view")
-            Button("Exit") {
-                router.pop()
+        NavigationStack(path: $router.path) {
+            List {
+                ForEach(goals) { goal in
+                    Button {
+                        router.push(.goalDetails(goal: goal))
+                    } label: {
+                        HStack {
+                            Text(goal.context)
+                                .font(.maintTextLight20)
+                            Spacer()
+                            Image(systemName: "chevron.right")
+                        }
+                        .padding(.vertical, 10)
+                    }
+                    .swipeActions(edge: .trailing, allowsFullSwipe: false) {
+                        Button {
+                            goalToDelete = goal
+                            showDeleteAlert = true
+                        } label: {
+                            Label("삭제", systemImage: "trash")
+                        }
+                        .tint(.red)
+                    }
+                }
             }
+            .listStyle(.plain)
+            
+            .alert("이 목표를 정말 삭제하시겠습니까?", isPresented: $showDeleteAlert, actions: {
+                Button("취소", role: .cancel) { }
+                Button("삭제", role: .destructive) {
+                    if let goal = goalToDelete {
+                        modelContext.delete(goal)
+                        goalToDelete = nil
+                    }
+                }
+            }, message: {
+                if let goal = goalToDelete {
+                    Text("\"\(goal.context)\" 목표가 완전히 삭제됩니다.")
+                }
+            })
+            
+            .navigationTitle("Goals")
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing, content: {
+                    Button {
+                        router.presentSheet(.goalCompose)
+                    } label: {
+                        Image(systemName: "square.and.pencil")
+                            .foregroundStyle(.mainBlue)
+                    }
+                })
+            }
+            
+            .navigationDestination(for: Route.self) { route in
+                switch route {
+                case .goalDetails(let goal):
+                    GoalDetailsView(goal: goal)
+                }
+            }
+        }
+        .sheet(item: $router.presentedSheet) { sheet in
+            GoalComposeView(router: $router)
         }
     }
 }
 
-//#Preview {
-//    BoardView()
-//}
+#Preview {
+    BoardView()
+}
