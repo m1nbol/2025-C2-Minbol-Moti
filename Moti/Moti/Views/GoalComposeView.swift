164879f659cbd917a70db87b0b5c7fb03fffc88f
BoMin <bmlee@postech.ac.kr>
2025-04-21 20:41:55 +0900

    ✨ Feat GoalComposeView

--- Moti/Moti/Views/GoalComposeView.swift
+++ Moti/Moti/Views/GoalComposeView.swift
@@ -6,17 +6,32 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct GoalComposeView: View {
     @Binding var router: NavigationRouter
     
+    @Environment(\.modelContext) private var modelContext
+    
+    @State private var goalContext: String = ""
+    @State private var dueDate: Date = Date()
+    
     var body: some View {
         NavigationStack {
-            VStack {
-                Text("enter your goal")
-                    .foregroundStyle(.mainGray)
+            VStack(spacing: 20) {
+                TextField("Enter your Goal", text: $goalContext)
+                    .autocorrectionDisabled()
                     .font(.mainTextSemiBold34)
+                HStack {
+                    DatePicker("", selection: $dueDate, displayedComponents: [.date])
+                        .tint(.mainBlue)
+                        .labelsHidden()
+                    Spacer()
+                }
+                Divider()
+                Text("You've selected a goal duration of \(dueDate.daysRemaining()) days")
             }
+            .padding(.horizontal, 20)
             .toolbar {
                 ToolbarItem(placement: .cancellationAction) {
                     Button {
@@ -26,11 +41,29 @@ struct GoalComposeView: View {
                             Image(systemName: "chevron.left")
                             Text("Goals")
                         }
-                        .foregroundStyle(.mainBlue)
+                    }
+                }
+                ToolbarItem(placement: .confirmationAction) {
+                    Button {
+                        saveGoal()
+                        router.dismissSheet()
+                    } label: {
+                        Text("Save")
                     }
                 }
             }
+            .foregroundStyle(.mainBlue)
         }
+    }
+    
+    private func saveGoal() {
+        let newGoal = Goal(
+            id: UUID(),
+            context: goalContext,
+            dueDate: dueDate,
+            createdAt: Date(timeIntervalSinceNow: 0)
+        )
+        modelContext.insert(newGoal)
     }
 }
 
